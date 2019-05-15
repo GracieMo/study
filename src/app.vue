1b71c1a58c79efc09fbbeee1c26627fb375998c4
GracieMo <loveyy397378357@163.com>
2019-05-16 00:12:22 +0800

    Signed-off-by: GracieMo <loveyy397378357@163.com>

--- src/app.vue
+++ src/app.vue
@@ -1,21 +1,46 @@
 <template>
-    <div id="test">{{text}}</div>
+    <div id="app">
+        <div id="cover">
+        </div>
+        <Header></Header>
+        <Todo></Todo>
+        <Footer></Footer>
+    </div>
 </template>
 
 <script>
+import Header from './todo/header.vue'
+import Footer from './todo/footer.jsx'
+import Todo from './todo/todo.vue'
+
     export default {
-        data(){
-            return{
-                text:'12324124'
-            };
+        components:{
+            Header,
+            Footer,
+            Todo
         }
+
     }
 </script>
 
-<style>
-    #test{
-        color:red;
-    }
+<style lang="stylus" scoped>
+#app{
+  position absolute
+  left 0
+  right 0
+  top 0
+  bottom 0
+}
+#cover{
+  position absolute
+  left 0
+  top 0
+  right 0
+  bottom 0
+  background-color #999
+  opacity .7
+  z-index -1
+}
 </style>
 
 
